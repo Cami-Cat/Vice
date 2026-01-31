0eb10ebb6c09aa16b71955b31d49626903432fbb
Cami-Cat <katten.en.vt@gmail.com>
2026-01-31 13:55:08 +0000

    Add avoidance and change velocity calculation

--- template/Scenes/Character/pawn_ai.gd
+++ template/Scenes/Character/pawn_ai.gd
@@ -6,11 +6,13 @@ signal idle_timeout()
 
 static var world_bounds : float = 100.0
 
-@export var ai_walk_speed : float = 5.0
+@export var ai_walk_speed : float = 2.0
+@export var max_idle_time : float = 15.0
 
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 @onready var pawn : Pawn = self.get_parent()
 var should_navigate : bool = true
+var just_idled : bool = false
 
 enum BEHAVIOUR {IDLE, WALK, FLEE}
 var current_behaviour : BEHAVIOUR
@@ -22,7 +24,11 @@ func _init() -> void:
 
 func choose_next_behaviour() -> void:
 	current_behaviour = BEHAVIOUR.IDLE
-	current_behaviour = randi_range(0, 2) as BEHAVIOUR
+	if !just_idled:
+		current_behaviour = randi_range(0, 1) as BEHAVIOUR
+	else:
+		just_idled = false
+		current_behaviour = BEHAVIOUR.WALK
 	print("%s Choosing Pawn behaviour: %s" % [pawn.name, BEHAVIOUR.keys()[int(current_behaviour)]])
 	behaviour_chosen.emit()
 	return
@@ -31,17 +37,25 @@ func _ready() -> void:
 	navigation_agent_3d.navigation_finished.connect(choose_next_behaviour)
 	pawn.is_possessed.connect(_pause_ai)
 	pawn.is_unpossessed.connect(_unpause_ai)
+	just_idled = true
 	choose_next_behaviour()
 	return
 
 func _do_behaviour() -> void:
 	match current_behaviour:
 		BEHAVIOUR.IDLE:
-			idle_timeout.emit()
+			_idle()
 		BEHAVIOUR.WALK:
 			_set_target_position()
 		BEHAVIOUR.FLEE:
 			idle_timeout.emit()
+	return
+
+func _idle() -> void:
+	var idle_time = randf_range(3.0, max_idle_time)
+	await get_tree().create_timer(idle_time).timeout
+	just_idled = true
+	idle_timeout.emit()
 	return
 
 func _pause_ai() -> void:
@@ -66,12 +80,19 @@ func _set_target_position() -> void:
 func _go_to_target_position() -> void:
 	var target_position = navigation_agent_3d.get_next_path_position()
 	var local_position = target_position - global_position
-	var direction = local_position.normalized()
+	var direction = (local_position.normalized() * ai_walk_speed)
 	
-	pawn.velocity = direction * ai_walk_speed
+	navigation_agent_3d.set_velocity(direction)
 	return
 
 func _physics_process(delta: float) -> void:
 	if !should_navigate : return
 	if current_behaviour == BEHAVIOUR.WALK || current_behaviour == BEHAVIOUR.FLEE:
 		_go_to_target_position()
+
+
+func _on_navigation_agent_3d_velocity_computed(safe_velocity: Vector3) -> void:
+	if !should_navigate : return
+	pawn.velocity = pawn.velocity.move_toward(safe_velocity, 0.75)
+	pawn.move_and_slide()
+	return
